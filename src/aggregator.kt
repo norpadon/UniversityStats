d8a2fe4e73e420acbaf956c875d9d8e310c23992
Artur Chahvadze <norpadon@yandex.ru>
2014-07-12 23:29:12 +0600

    Провел рефакторинг, учел целевиков и льготников

--- src/aggregator.kt
+++ src/aggregator.kt
@@ -2,47 +2,81 @@ package aggregators
 
 import java.util.Random
 
-public class Record (val Name : String, val Score : Int, val olymp : Boolean = false, val Priority : Int = 0) {
+/*
+ * Класс записи в таблице
+ */
+public class Record (val Name: String, val Score: Int,
+                     val Olymp: Boolean = false, val Priority: Int = 0,
+                     val Targeted: Boolean = false, val Exempt: Boolean = false) {
 
 }
 
+/*
+ * Aggregator осуществляет получение данных по направлению подготовки
+ *  и исполльзуется для рассчета конкурсной статистики.
+ */
 public trait Aggregator {
-    fun getRecords() : List<Record>
 
-    val Name : String
-    val PlacesCount : Int
-    val Reserve : Int
+    fun getRecords(): List<Record> // Возвращает таблицу заявлений на данное направление
 
+    val Name: String       // Название направления подготовки.
+    val PlacesCount: Int   // Количество бюджетных мест.
+    val Targeted: Int      // Из них целевиков.
+    val Exempts: Int       // Из них льготников.
+    val Reserve: Int       // Размер резерва на случай если олимпиадников больше чем мест.
+
+    // Возвращает средний балл абитуриентов.
     fun getAverageScore() : Double {
         val rs = getRecords()
         return rs.fold(0) { (a, b) -> a + b.Score } / rs.size.toDouble()
     }
 
-    fun getStandartDeviation() : Double {
+    // Возвращает среднеквадратичное отклонение балла.
+    fun getStandardDeviation(): Double {
         val rs = getRecords()
         val avg = getAverageScore()
         return Math.sqrt(rs.fold(0.0) { (a, b) -> a + (avg - b.Score) * (avg - b.Score) } / rs.size.toDouble())
     }
 
-    fun getMaxMinScore() : Int {
-        return gms(getRecords())
+    // Возвращает максимально возможный проходной балл (проходной балл первой волны).
+    fun getMaxPassingScore(): Int {
+        return auxSelectiveScoreCalc(getRecords())
     }
 
-    fun getMinMinScore() : Int {
-        return gms(getRecords() filter { it.Priority == 0 })
+    // Возвращает минимально возможный проходной балл (учитываются только оригиналы аттестатов).
+    fun getMinPassingScore(): Int {
+        return auxSelectiveScoreCalc(getRecords() filter { it.Priority == 0 })
     }
 
-    fun getRealMinScore() : Int {
-        return gms(getRecords() filter { it.Priority == 0 || Random().nextInt() % 3 == 0 })
+    // Возвращает ожидаемый проходной балл, основанный на предположении о том что верятность поступления
+    //  абитуриента с копией аттестата равна 1/3 (В среднем 3 топовых вуза на направление подготовки).
+    fun getMeanPassingScore(): Int {
+        return auxSelectiveScoreCalc(getRecords() filter { it.Priority == 0 || Random().nextInt() % 3 == 0 })
     }
 
-    private fun gms(data : List<Record>) : Int {
+    // Вспомогательная функция для рассчета проходного балла по заданной выборке.
+    private fun auxSelectiveScoreCalc(data: List<Record>): Int {
+        // Костыль для тех направлений, где нет бюджетных мест (Проргамма по экономике ВШЭ и Лондонского университета).
         if (PlacesCount == 0) {
-            return Integer.MAX_VALUE
+            return -1
         }
-        val sorted = (data sortDescendingBy { it.Score + if (it.olymp) 400 else 0 }).toArrayList()
-        val olympcnt = data count { it.olymp }
+
+        // Считаем количество олимпиадников
+        val olympcnt = data count { it.Olymp }
+
+        // Переменные для хранения количества мест, оставшихся от льготников и целевиков
+        var exemptcnt = Exempts
+        var targetedcnt = Targeted
+
+        // Получаем множество конкурсантов, отсортированных по убыванию балла ЕГЭ + БВИ,
+        //  отчитываем льготников и целевиков в пределах квоты и сортируем полученный список снова.
+        val sorted = data sortDescendingBy
+        { it.Score + if (it.Olymp) 400 else 0 } map
+        { Pair(it, it.Olymp || it.Targeted && targetedcnt-- > 0 || it.Exempt && exemptcnt-- > 0) } sortDescendingBy
+        { it.first.Score + if (it.second) 400 else 0 }
+        // Находим последнего зачисленного с учетом резерва
         var r = Math.min(sorted.size, Math.max(PlacesCount, olympcnt + Reserve)) - 1
-        return sorted[r].Score
+        // Возвращаем его балл
+        return sorted[r].first.Score
     }
 }
--- src/hse.kt
+++ src/hse.kt
@@ -2,60 +2,90 @@ package aggregators
 
 import jxl.*
 import java.net.*
-import java.io.BufferedInputStream
 import java.util.ArrayList
-import java.text.*
 import java.util.regex.*
-import sun.misc.IOUtils
-import java.util.Scanner
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.TreeMap
 import java.util.concurrent.Executors
 import sun.awt.Mutex
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.Future
 
+// Страница со списком направлений подготовки.
 private val progPage = BufferedReader(InputStreamReader(
         (URL("http://ba.hse.ru/base2014").openConnection() as HttpURLConnection).getInputStream()!!)).readText()
+// Страница с информацией о количестве мест.
 private val placesPage = BufferedReader(InputStreamReader(
         (URL("http://ba.hse.ru/kolmest2014").openConnection() as HttpURLConnection).getInputStream()!!)).readText()
 
+// Отображение (направление подготовки -> количество бюджетных мест).
 private val placesMap = TreeMap<String, Int>()
+// Отображение (направление подготовки -> квота целевиков).
+private val targetedMap = TreeMap<String, Int>()
+// Отображение (направление подготовки -> квота льготников).
+private val exemptsMap = TreeMap<String, Int>()
 
+// Пул потоков для асинхронного получения и обработки данных.
 private val exec = Executors.newCachedThreadPool()
 
+/*
+ * Обобщенный агрегатор для Высшей Школы Экономики.
+ * Конструктор принимает адрес excel таблицы с данными
+ */
 class HseAggregator(programUrl : String) : Aggregator {
-    val conn = URL(programUrl).openConnection() as HttpURLConnection
-    val dataStream = conn.getInputStream()
-    val ss = Workbook.getWorkbook(dataStream)!!.getSheet(0)!!
+    // Получение  таблицы
+    private val conn = URL(programUrl).openConnection() as HttpURLConnection
+    private val dataStream = conn.getInputStream()
+    private val ss = Workbook.getWorkbook(dataStream)!!.getSheet(0)!!
+    {
+        // Заполнение таблицы мест в случае если она еще не заполнена.
+        if (placesMap.size == 0) {
+            loadPlacesCount()
+        }
+    }
 
+    // Вспомогатеьная функция получнения ячейки таблицы
     private fun cell(column : Int, row : Int) : String {
         return ss.getCell(column, row)!!.getContents()!!
     }
 
+    // Название направления подготовки получаем кавычек внутри ячейки A2.
     override val Name = cell(0, 1).split("\"")[1]
 
-    val dataList = ArrayList<Record>();
+    // Получаем список абитуриентов.
+    private val dataList = ArrayList<Record>();
     {
+        // Еслина направление нужно сдавать 3 экзамена, в ячейке L5 находится строка "Сумма баллов".
         val subjcount = if (cell(10, 4) == "Сумма баллов") 3 else 4
+        // Таблица начинается с седьмой строки.
         var curRow = 6
+        // Заполняем таблицу пока можем.
         while (curRow < ss.getRows() && cell(2, curRow) != "") {
+            // Имя.
             val name = cell(2, curRow)
-            var scs = ""
+            // Буфер для хранения фигни.
+            var scs: String
+            // Буфер для хранения суммарного балла.
             var score = 0
+            // Считаем суммарный балл.
             for (i in 7..6 + subjcount) {
                 scs = cell(i, curRow)
                 score += if (scs != "") Integer.parseInt(scs) else 0
             }
+            // Считаем приоритет.
             val prior = if (cell(3, curRow) == "Подлинник") 0 else 1
+            // Смотрим на олимпиады.
             val olymp = cell(4, curRow) != ""
+            // Сохраням полученные данные.
             dataList.add(Record(name, score, olymp, prior))
             ++curRow
         }
     }
 
+    //Тут все ясно.
     override val PlacesCount = placesMap[Name]!!
+    override val Exempts = exemptsMap[Name]!!
+    override val Targeted = targetedMap[Name]!!
     override val Reserve = 25
 
     override fun getRecords() : List<Record> {
@@ -63,53 +93,71 @@ class HseAggregator(programUrl : String) : Aggregator {
     }
 }
 
+// Треш, угар, и содомия.
 private fun loadPlacesCount() {
     val regex = Pattern.compile(
-            """<td>\s*<a href="[^<>]*">([^<>]*)</a>((<br>)|[^<>])*</td>\s*<td style="[^<>]*">(\d+)</td>"""
+            """<td>\s*<a href="[^<>]*">([^<>]*)</a>((<br>)|[^<>])*</td>""" +
+            """\s*<td style="[^<>]*">(&nbsp;)?(\d*)</td>""" +
+            """\s*<td style="[^<>]*">(&nbsp;)?(\d*)</td>""" +
+            """\s*<td style="[^<>]*">(&nbsp;)?(\d*)</td>"""
     )
 
     val m = regex.matcher(placesPage)
     while (m.find()) {
-        placesMap.set(m.group(1)!!, Integer.parseInt(m.group(4)!!));
+        placesMap.set(m.group(1)!!, Integer.parseInt(m.group(5)!!));
+        targetedMap.set(m.group(1)!!, if (m.group(7)!! != "") Integer.parseInt(m.group(7)!!) else 0);
+        exemptsMap.set(m.group(1)!!, if (m.group(9)!! != "") Integer.parseInt(m.group(9)!!) else 0);
     }
 
     placesMap.set("Программа двух дипломов по экономике НИУ ВШЭ и Лондонского университета", 0)
+    targetedMap.set("Программа двух дипломов по экономике НИУ ВШЭ и Лондонского университета", 0)
+    exemptsMap.set("Программа двух дипломов по экономике НИУ ВШЭ и Лондонского университета", 0)
 }
 
+// Возвращает арргеторы для всех направлений подготовки ВШЭ.
 private fun getAllHseAggregators() : List<HseAggregator> {
-    loadPlacesCount()
 
+    // Страшное регулярное выражения для поиска ссылок на excel файлы.
     val regex = Pattern.compile("""href="([^"]*.xls)"""")
     val m = regex.matcher(progPage)
 
+    // Как ни странно, результат.
     val result = ArrayList<HseAggregator>();
+    // Мьютекс для синхронизации доступа к массиву.
     val mutex = Mutex()
+    // Список задач, завершения котрых нажно дождаться.
     val fs = ArrayList<Future<*>>()
+    //Ищем ссылки
     while (m.find()) {
         val g = m.group(1)!!
+        // И просим наш пул потоков их обработать.
         fs.add(exec.submit {
             val res = HseAggregator(g)
+            // Вот это нужно, чтобы избежать гонки данных при записи результата.
             mutex.lock()
             result.add(res)
             mutex.unlock()
         })
     }
 
+    // Ждем завершения обработки.
     for (f in fs) {
         f.get()
     }
 
+    // Возвращаем резульат.
     return result
 }
 
+// Печатает статистику.
 fun printData() {
     val m = Mutex()
     val fs = ArrayList<Future<*>>()
     for (hse in getAllHseAggregators()) {
         fs.add(exec.submit() {
-            val minmax = hse.getMaxMinScore()
-            val minmin = hse.getMinMinScore()
-            val avgmin = hse.getRealMinScore()
+            val minmax = hse.getMaxPassingScore()
+            val minmin = hse.getMinPassingScore()
+            val avgmin = hse.getMeanPassingScore()
 
             val recs = hse.getRecords()
 
@@ -120,11 +168,11 @@ fun printData() {
             System.out.print("Количество заявлений: ")
             System.out.println(recs.size)
             System.out.print("Из них олимпиадников: ")
-            System.out.println(recs.filter { it.olymp } .size)
+            System.out.println(recs.filter { it.Olymp } .size)
             System.out.print("Средний балл абитуриентов: ")
             System.out.println(java.lang.String.format("%.2f", hse.getAverageScore()))
             System.out.print("Среднеквадратичное отклонение: ")
-            System.out.println((java.lang.String.format("%.2f", hse.getStandartDeviation())))
+            System.out.println((java.lang.String.format("%.2f", hse.getStandardDeviation())))
             System.out.print("Ожидаемый проходной балл первой волны: ")
             System.out.println(minmax)
             System.out.print("Вероятный проходной балл второй волны: ")
